463cfa1333a4c1a9bdf09a29f5e9ba972831d2b4
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-08-01 01:13:39 +0100

    WIP: Decoupled StatusBar in TradingsPage and fetching InstrumentSummaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-
 import TradingPage from './pages/TradingPage'
+import UserOverviewPage from './pages/UserOverviewPage'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<TradingPage />} />
+                <Route path="/user" element={<UserOverviewPage />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/UserOverviewPage.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const UserOverviewPage: FC = () => {
+    return <h1>User Page</h1>
+}
+
+export default UserOverviewPage
